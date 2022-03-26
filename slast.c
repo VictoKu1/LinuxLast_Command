84a8fee09fabb826670c2555915a53c1015aac9c
Andrey Bakhrakh <andreyba2000@gmail.com>
2022-03-26 12:16:01 +0300

    Done

--- slast.c
+++ slast.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 
 void showtime(long time){
     char  *cp;
@@ -11,49 +12,53 @@ void showtime(long time){
     printf("%12.12s", cp+4);
 }
 
-void show_info(struct utmp *wtbufp){
+int show_info(struct utmp *wtbufp){
     if(wtbufp->ut_type == BOOT_TIME || wtbufp->ut_type == USER_PROCESS){
-        printf("user: %s ", wtbufp->ut_user);
-        printf("host: %s ", wtbufp->ut_host);
-        printf("exit: %d ", wtbufp->ut_exit.e_termination);
-        // switch (wtbufp->ut_type)
-        // {
-        // case BOOT_TIME:
-        //     printf("system boot");
-        //     break;
-        
-        // default:
-        //     printf("line: %s", wtbufp->ut_line);
-        //     break;
-        // }
-        printf("add_v6: %ls ", wtbufp->ut_addr_v6);
-        printf("line: %s ", wtbufp->ut_line);
-        printf("id: %s ", wtbufp->ut_id);
-        printf("pid: %d ", wtbufp->ut_pid);
-        printf("session: %d ", wtbufp->ut_session);
-        printf("type: %d ", wtbufp->ut_type);
-        // printf("%d ", wtbufp->ut_tv.tv_sec);
+        printf("%s ", wtbufp->ut_user);
+        if(!strcmp(wtbufp->ut_line, "~")){
+            printf("system boot ");
+        }
+        else{
+            printf("%s ", wtbufp->ut_line);
+        }
+        printf("%s ", wtbufp->ut_host);
         showtime((long)wtbufp->ut_tv.tv_sec);
         printf("\n");
+        return 1;
     }
-    
+    return 0;
 }
 
 
 int main(int argc, char const *argv[])
 {
     struct utmp current_record;
-    int wtmpfd, utmpfd;
     int reclen = sizeof(current_record);
 
-    if((wtmpfd = open(WTMP_FILE, O_RDONLY)) == -1){
+    FILE* fp;
+    if((fp = fopen(WTMP_FILE, "r")) == NULL){
         perror(WTMP_FILE);
         exit(1);
     }
-    while (read(wtmpfd, &current_record,reclen) == reclen)
-    {
-        show_info(&current_record);
+    if(fseek(fp, reclen, SEEK_END) == -1){
+        exit(1);
     }
-    close(wtmpfd);
+    if(argc == 1){
+        while(fseek(fp, -2 * reclen, SEEK_CUR) != -1){
+            fread(&current_record, reclen, 1, fp);
+            show_info(&current_record);
+        }
+    }
+    else{
+        int i = atoi(argv[1]);
+        while(fseek(fp, -2 * reclen, SEEK_CUR) != -1 && i > 0){
+            fread(&current_record, reclen, 1, fp);
+            if(show_info(&current_record)){
+                i--;
+            }
+        }
+    }    
+
+    fclose(fp);
     return 0;
 }
